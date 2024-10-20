ba96b456f9119b1544e41de8f8b43cbffe55db2f
Jrondero09 <143197300+Jrondero09@users.noreply.github.com>
2024-10-20 20:42:57 +0800

    LLLLLLLLLOL

--- Desktop/API-MAIN/branches/branch.controller.js
+++ Desktop/API-MAIN/branches/branch.controller.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const branchService = require('./branch.service');
 
+//ROUTES
 router.get('/', getAllBranch);
 router.get('/:id', getBranchById);
 router.post('/', createBranch);
